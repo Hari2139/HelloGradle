d47be52d0e002efc8f028f9840fc82c20bc6f9f7
Hari Samala <hari2139@gmail.com>
2014-09-23 17:59:39 -0700

    Initial commit

---
+++ src/main/java/com/hari/Main.java
@@ -0,0 +1,13 @@
+package com.hari;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Person p = new Person("Hari");
+		System.out.println(p.toString() + " is awesome!");
+	}
+
+}
